0e24d3dd2224f3023406c12cbe64f830ec5c5bdf
PatrickVibild <44056401+PatrickVibild@users.noreply.github.com>
2018-10-26 10:45:54 +0200

    Rename the class to MortenCombat

--- LevelScreen.java
+++ LevelScreen.java
@@ -1,7 +1,18 @@
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapProperties;
+
 public class LevelScreen extends BaseScreen
 {
     public void initialize() 
-    {        
+    {
+       /** TilemapActor tma = new TilemapActor("assets/map.tmx", mainStage);
+        for (MapObject obj : tma.getRectangleList("Solid") )
+        {
+            MapProperties props = obj.getProperties();
+            new Solid( (float)props.get("x"), (float)props.get("y"),
+                    (float)props.get("width"), (float)props.get("height"),
+                    mainStage );
+        }*/
         
     }
 
