798bfdc48d11d1f189e866ab85bc9066a6270a91
PatrickVibild <44056401+PatrickVibild@users.noreply.github.com>
2018-10-26 12:08:53 +0200

    implementing Sara

--- LevelScreen.java
+++ LevelScreen.java
@@ -1,10 +1,15 @@
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+
 
 public class LevelScreen extends BaseScreen
 {
+    Hero sara;
     public void initialize() 
     {
+
        TilemapActor tma = new TilemapActor("assets/map.tmx", mainStage);
        for (MapObject obj : tma.getRectangleList("Solid") )
         {
@@ -13,11 +18,27 @@ public class LevelScreen extends BaseScreen
                     (float)props.get("width"), (float)props.get("height"),
                     mainStage );
         }
+        MapObject startPoint = tma.getRectangleList("start").get(0);
+        MapProperties startProps = startPoint.getProperties();
+        sara = new Hero( (float)startProps.get("x"), (float)startProps.get("y"), mainStage);
         
     }
 
     public void update(float dt)
     {
+        // hero movement controls
+        if (Gdx.input.isKeyPressed(Keys.LEFT))
+            sara.accelerateAtAngle(180);
+        if (Gdx.input.isKeyPressed(Keys.RIGHT))
+            sara.accelerateAtAngle(0);
+        if (Gdx.input.isKeyPressed(Keys.UP))
+            sara.accelerateAtAngle(90);
+        if (Gdx.input.isKeyPressed(Keys.DOWN))
+            sara.accelerateAtAngle(270);
+        for (BaseActor solid : BaseActor.getList(mainStage, "Solid"))
+        {
+            sara.preventOverlap(solid);
+        }
        
     }
 }
