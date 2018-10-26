c7b9cca0a56a20e13a59fe97dcac0b759bceeb6a
PatrickVibild <44056401+PatrickVibild@users.noreply.github.com>
2018-10-26 12:09:20 +0200

    implementing Sara

---
+++ Hero.java
@@ -0,0 +1,46 @@
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.utils.Array;
+
+
+public class Hero extends BaseActor{
+
+
+    public Hero(float x, float y, Stage s)
+    {
+        super(x,y,s);
+        String fileName = "assets/character.png";
+       // int rows = 4;
+        //int cols = 4;
+        Texture texture = new Texture(Gdx.files.internal(fileName), true);
+        /**int frameWidth = texture.getWidth() / cols;
+        int frameHeight = texture.getHeight() / rows;
+        float frameDuration = 0.2f;
+        TextureRegion[][] temp = TextureRegion.split(texture, frameWidth, frameHeight);
+        Array<TextureRegion> textureArray = new Array<TextureRegion>();
+        for (int c = 0; c < cols; c++)
+            textureArray.add( temp[0][c] );
+        /**south = new Animation(frameDuration, textureArray, Animation.PlayMode.LOOP_PINGPONG);
+        textureArray.clear();
+        for (int c = 0; c < cols; c++)
+            textureArray.add( temp[1][c] );
+        west = new Animation(frameDuration, textureArray, Animation.PlayMode.LOOP_PINGPONG);
+        textureArray.clear();
+        for (int c = 0; c < cols; c++)
+            textureArray.add( temp[2][c] );
+        east = new Animation(frameDuration, textureArray, Animation.PlayMode.LOOP_PINGPONG);
+        textureArray.clear();
+        for (int c = 0; c < cols; c++)
+            textureArray.add( temp[3][c] );
+        north = new Animation(frameDuration, textureArray, Animation.PlayMode.LOOP_PINGPONG);
+        setAnimation(south);
+        facingAngle = 270;*/
+        setBoundaryPolygon(8);
+        setAcceleration(400);
+        setMaxSpeed(200);
+        setDeceleration(400);
+    }
+}
