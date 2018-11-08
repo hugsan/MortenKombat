eb5c513f46ca4eab05b8e35ccf6a553c428d7d03
Jens Glahn <jensglahn96@gmail.com>
2018-11-08 13:46:10 +0100

    Implemented Chest class. Jens

--- src/core/actors/fight/Fighter.java
+++ src/core/actors/fight/Fighter.java
@@ -12,12 +12,10 @@ public class Fighter {
     public void attachTwo(){
         hp = hp + 10;
     }
-    public int getHp() {
-        return hp;
+    public int getHp() { return hp;
     }
 
-    public void setHp(int hp) {
-        this.hp = hp;
+    public void setHp(int hp) { this.hp = hp;
     }
 
 }
