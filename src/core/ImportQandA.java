a756de94fe97b73969bd453535112850ae7b2131
mbalat17 <mbalat17@student.aau.dk>
2018-11-13 15:49:03 +0100

    improved ImportQnA function II

--- src/core/ImportQandA.java
+++ src/core/ImportQandA.java
@@ -1,53 +1,59 @@
 package core;
 
-import java.io.*;
-import java.util.*;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class ImportQandA {
 
 
-
-        public String number;
-        public String question;
-        public String correctAnswer;
-        public String wrongAnswer1;
-        public String wrongAnswer2;
-        public String wrongAnswer3;
-
-
-        File data = new File("assets\\QnAData.txt");
-
-        public ImportQandA(int numOfQ) throws FileNotFoundException{
+    public String number;
+    public String question;
+    public String correctAnswer;
+    public String wrongAnswer1;
+    public String wrongAnswer2;
+    public String wrongAnswer3;
 
 
-            Scanner dataReader = new Scanner(data);
+    public ImportQandA(int numOfQ, File data) throws FileNotFoundException {
 
 
+        Scanner dataReader = new Scanner(data);
 
-            while (dataReader.hasNextLine()) {
+
+        while (dataReader.hasNextLine()) {
 
 
-                number = "p00" + numOfQ;
-                if (number.equals(dataReader.next())) {
+            number = "p00" + numOfQ;
+            if (number.equals(dataReader.next())) {
 
-                    dataReader.nextLine();
-                    this.question = dataReader.nextLine();
-                    System.out.println(question);
-                    this.correctAnswer = dataReader.nextLine();
-                    System.out.println(correctAnswer);
-                    this.wrongAnswer1 = dataReader.nextLine();
-                    System.out.println(wrongAnswer1);
-                    this.wrongAnswer2 = dataReader.nextLine();
-                    System.out.println(wrongAnswer2);
-                    this.wrongAnswer3 = dataReader.nextLine();
-                    System.out.println(wrongAnswer3);
-
-                }
+                dataReader.nextLine();
+                this.question = dataReader.nextLine();
+                System.out.println(question);
+                this.correctAnswer = dataReader.nextLine();
+                System.out.println(correctAnswer);
+                this.wrongAnswer1 = dataReader.nextLine();
+                System.out.println(wrongAnswer1);
+                this.wrongAnswer2 = dataReader.nextLine();
+                System.out.println(wrongAnswer2);
+                this.wrongAnswer3 = dataReader.nextLine();
+                System.out.println(wrongAnswer3);
 
             }
 
         }
+
+    }
+
+    public static void filler(int qNumber, ArrayList<ImportQandA> arrayList, File data) throws FileNotFoundException {
+
+        for (int i= 1; i<= qNumber; i++) {
+            arrayList.add(new ImportQandA(i, data));
+        }
+    }
+
+
 }
 
--- src/core/MortenCombat.java
+++ src/core/MortenCombat.java
@@ -8,12 +8,46 @@ import core.screen.LevelScreen;
 import core.screen.MainMenuScreen;
 import core.screen.MapLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class MortenCombat extends BaseGame {
     static private testingFigther fighter = new testingFigther();
     static LevelScreen[] layout = new LevelScreen[MapLayout.values().length];
 
+    public void importing() throws FileNotFoundException {
+
+
+        File data = new File("assets\\QnA\\QnAData.txt");
+        File data2 = new File("assets\\QnA\\Science.txt");
+        File data3 = new File("assets\\QnA\\Geography.txt");
+        File data4 = new File("assets\\QnA\\History.txt");
+        File data5 = new File("assets\\QnA\\Art.txt");
+        File data6 = new File("assets\\QnA\\Sport.txt");
+        File data7 = new File("assets\\QnA\\Entertainment.txt");
+
+
+        ArrayList<ImportQandA> listQScience = new ArrayList<ImportQandA>();
+        ArrayList<ImportQandA> listQGeography = new ArrayList<ImportQandA>();
+        ArrayList<ImportQandA> listQHistory = new ArrayList<ImportQandA>();
+        ArrayList<ImportQandA> listQArt = new ArrayList<ImportQandA>();
+        ArrayList<ImportQandA> listQSport = new ArrayList<ImportQandA>();
+        ArrayList<ImportQandA> listQEntertainment = new ArrayList<ImportQandA>();
+
+        ImportQandA.filler(5,listQScience,data2);
+
+        ImportQandA a = listQScience.get(2);
+        System.out.println(a.question);
+    }
+
     public void create() {
         super.create();
+        try {
+            importing();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         int i = 0;
 
