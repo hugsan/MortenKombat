f645b98e8d391ed3ad2c5b30e728f0f9ace203d1
Mads Herlevsen <Mads.her@hotmail.com>
2018-11-15 13:29:37 +0100

    Updated to the character selection screen. Mads.

--- src/core/framework/SelectionArea.java
+++ src/core/framework/SelectionArea.java
@@ -4,18 +4,19 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class SelectionArea extends DropTargetActor {
 
-    private int row;
-    private int col;
+    private int heroNumber; //fighter = 1, mage = 2, support = 3.
 
     public SelectionArea(float x, float y, Stage s) {
 
         super(x, y, s);
-        //loadTexture("a");
+        isTargetable();
+        loadTexture("assets/img/buttons/back.png");
+        setSize(100,100);
+        setBoundaryRectangle();
+
     }
 
-    public void setRow(int r)    {  row = r;  }
-    public void setCol(int c)    {  col = c;  }
-    public int getRow()    {  return row;  }
-    public int getCol()    {  return col;  }
+    public int getHeroNumber()                  { return heroNumber; }
+    public void setHeroNumber(int heroNumber)   { this.heroNumber = heroNumber; }
 
 }
