c002abee10c1f9950f18a2526cbf7f882841a7ac
PatrickVibild <44056401+PatrickVibild@users.noreply.github.com>
2018-11-15 23:35:37 +0100

    Deleted AOE dmg interface. not needed.

--- src/core/actors/fightingactors/AreaDamageAbility.java
+++
@@ -1,6 +0,0 @@
-package core.actors.fightingactors;
-
-public interface AreaDamageAbility {
-    public boolean attackThree(Fighter fighter, Fighter fighter2);
-    public boolean attackThree(Fighter fighter, Fighter fighter2, Fighter fighter3);
-}
